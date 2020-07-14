3315f66eae1972c14f10ee7f95185142a7fb5463
alex <you@example.com>
2020-07-14 02:22:57 -0300

    Only enable place_type value as coutry, region or city

--- app/static/index.js
+++ app/static/index.js
@@ -4,20 +4,26 @@ function getPlaceType(){
 
   if(placeType === "city"){
     let regionDiv = document.createElement("div");
+
     regionDiv.id = "region_div";
+    regionDiv.className = "locations_structured_fields";
     regionDiv.innerHTML = '<br><label for = "region">Region:</label><br><input id = "region" placeholder = "Examplo: São Paulo" required>';
 
     structuredLocationDiv.appendChild(regionDiv);
 
     let cityDiv = document.createElement("div");
+
     cityDiv.id = "city_div";
+    cityDiv.className = "locations_structured_fields";
     cityDiv.innerHTML = '<br><label for = "city">City:</label><br><input id = "city" placeholder = "Examplo: São Paulo">';
 
     structuredLocationDiv.appendChild(cityDiv);
 
   }else if(placeType === "region"){
     let regionDiv = document.createElement("div");
+
     regionDiv.id = "region_div";
+    regionDiv.className = "locations_structured_fields";
     regionDiv.innerHTML = '<br><label for = "region">Region:</label><br><input id = "region" placeholder = "Examplo: São Paulo" required>';
 
     structuredLocationDiv.appendChild(regionDiv);
@@ -33,3 +39,15 @@ function getPlaceType(){
 }
 
 document.getElementById("place_type").addEventListener("input", getPlaceType);
+
+function checkPlaceType(){
+  let placesTypes = ["country", "region", "city"];
+  placeType = document.getElementById("place_type").value.toLowerCase();
+
+  if(placesTypes.includes(placeType)){
+    return true;
+  }
+  alert("Place Type value must be country, region or city");
+  return false;
+}
+
