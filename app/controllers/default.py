60fd635e692de9d3c762b83f343ff4626373b8f0
alex <you@example.com>
2020-07-14 13:23:43 -0300

    Get all input values in the index function

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,22 @@
+from flask import render_template, request
 from app import app
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return 'Hello World'
+
+    if request.method == 'POST':
+        country      = request.form['country']
+        country_code = request.form['country_code']
+        place_type   = request.form['place_type']
+
+        '''
+        place_name   = request.form['name']
+        place_code   = request.form['place_code']
+        area_code    = request.form['area_code']
+        nicknames    = request.form['nicknames']
+        gentile      = request.form['gentile']
+        '''
+
+    return render_template('index.html')
+
